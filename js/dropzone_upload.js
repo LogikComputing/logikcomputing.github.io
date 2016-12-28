9fedda37f2d31957ef07e319bc23847fa249ab5c
Philip Bale <pbale95@gmail.com>
2016-12-27 19:01:02 -0500

    App Icon generator

--- js/dropzone_upload.js
+++ js/dropzone_upload.js
@@ -4,96 +4,53 @@ $(document).ready(function() {
     // grap our upload form by its id
     $(".icon-upload").each(function() {
         console.log("Applying dropzone");
-        $(this).dropzone({
+        var dropzone = $(this).dropzone({
             // restrict image size to a maximum 1MB
             maxFilesize: 1,
-            // changed the passed param to one accepted by
-            // our rails app
-            paramName: "upload[image]",
             // show remove links on each image upload
-            addRemoveLinks: true,
-            // if the upload was successful
-            success: function(file, response) {
-                // find the remove button link of the uploaded file and give it an id
-                // based of the fileID response from the server
-                console.log("Image success uploaded" + response.url);
-                $(file.previewTemplate).find('.dz-remove').attr('id', response.fileID);
-                // add the dz-success class (the green tick sign)
-                $(file.previewElement).addClass("dz-success");
-                updateIconImages(response.url);
+            addRemoveLinks: false,
+
+            accept: function (file, done) {
+                done('Here you can write anything');
+                $('.dz-preview').last().toggleClass('dz-error dz-success');
+                console.log("Icon accepted!");
+                setTimeout(function() {generateImages(); }, 200);
             },
-            //when the remove button is clicked
-            removedfile: function(file) {
-                // grap the id of the uploaded file we set earlier
-                var id = $(file.previewTemplate).find('.dz-remove').attr('id');
-                // make a DELETE ajax request to delete the file
-                $.ajax({
-                    type: 'DELETE',
-                    url: '/uploads/' + id,
-                    success: function(data) {
-                        console.log(data.message);
-                    }
-                });
-            }
+        });
+
+        dropzone.on('addedfile', function (file) {
+            console.log("Yoo");
         });
     });
 
-    function updateIconImages(url) {
-        $('img').each(function() {
-            var src = $(this).attr('src');
-            var name = $(this).attr('name');
-            var updated_url = url.replace('original', name);
-            if (src.indexOf('place') >= 0) {
-                // awesome pony detection algorithm triggered!
-                $(this).attr('src', updated_url);
-            }
-        });
-    };
-    $(".screenshot-upload").each(function() {
-        $(this).dropzone({
-            // restrict image size to a maximum 1MB
-            maxFilesize: 1,
-            // changed the passed param to one accepted by
-            // our rails app
-            paramName: "upload[image]",
-            previewsContainer: ".screenshot-preview",
-            // show remove links on each image upload
-            addRemoveLinks: true,
-            success: function(file, response) {
-                // find the remove button link of the uploaded file and give it an id
-                // based of the fileID response from the server
-                console.log("Image success uploaded" + response.url + ', slot: ' + response.slot);
-                $(file.previewTemplate).find('.dz-remove').attr('id', response.fileID);
-                // add the dz-success class (the green tick sign)
-                $(file.previewElement).addClass("dz-success");
-                updateScreenshotImages(response.url, response.slot);
-            },
-            //when the remove button is clicked
-            removedfile: function(file) {
-                // grap the id of the uploaded file we set earlier
-                var id = $(file.previewTemplate).find('.dz-remove').attr('id');
-                // make a DELETE ajax request to delete the file
-                $.ajax({
-                    type: 'DELETE',
-                    url: '/uploads/' + id,
-                    success: function(data) {
-                        console.log(data.message);
-                    }
-                });
-            }
-        });
-    });
+    function generateImages() {
+        var baseImg = $('.dz-image').children('img')[0]; 
+        console.log("Src: " + baseImg.src);
 
-    function updateScreenshotImages(url, slot) {
-        $('img').each(function() {
-            var src = $(this).attr('src');
-            var image_slot = $(this).attr('slot');
-            var size = $(this).attr('name')
-            var updated_url = url.replace('original', size);
-            if (image_slot && image_slot.indexOf(slot) >= 0) {
-                // awesome pony detection algorithm triggered!
-                $(this).attr('src', updated_url);
+        var sizes = [29, 40, 48, 50, 57, 60, 72, 76, 83.5];
+        var sizeMultipliers = [1, 1.5, 2, 3, 4];
+
+        for (var i = 0; i < sizes.length; i++) {
+            for (var j = 0; j < sizeMultipliers.length; j++) {
+                var name = sizes[i] + '@' + sizeMultipliers[j];
+                console.log("Name: " + name);
+                var matches = $("img[name='"+ name + "']");
+
+                if (matches.size() > 0) {
+                    var canvas = document.createElement('canvas');
+                    var ctx = canvas.getContext('2d');
+                    var len = sizes[i] * sizeMultipliers[j];
+                    canvas.width = len;
+                    canvas.height = len;
+
+                    ctx.drawImage(baseImg, 0, 0, len, len);
+                    var url = canvas.toDataURL();
+
+                    matches.each(function() {
+                        this.src = url; 
+                    });
+                }
             }
-        });
-    };
+        }
+    }
 });
